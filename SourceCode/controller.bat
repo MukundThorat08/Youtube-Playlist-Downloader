5c379aa5bb091fa445dbcfb4b46293fcf9ce50d8
MukundThorat <mukundthorat08@gmail.com>
2024-02-08 14:29:29 +0530

    fixes bugs

--- SourceCode/controller.bat
+++ SourceCode/controller.bat
@@ -2,33 +2,20 @@
 color A
 echo """"""                                                                                                     """"""
 echo "                                                                                                               "
-ping 127.0.0.1 -n 1.0 > nul
-echo "         ________                __________ ____            ____ ___   _______        ___ _________            "
-ping 127.0.0.1 -n 1.1 > nul
-echo "        |    ____|              /    __    \\   \          /   /|   | |       \      |   |    __    \          "
-ping 127.0.0.1 -n 1.2 > nul
-echo "        |   |       ___________|    /  \    |\   \        /   / |   | |    _   \     |   |   |   \   \         "
-ping 127.0.0.1 -n 1.3 > nul
-echo "        |   |      |    ___    |   |    |   | \   \      /   /  |   | |   | \   \    |   |   |    |   |        "
-ping 127.0.0.1 -n 1.4 > nul
-echo "        |   |      |   |   |   |   |    |   |  \   \    /   /   |   | |   |  \   \   |   |   |    |   |        "
-ping 127.0.0.1 -n 1.5 > nul
-echo "        |   |      |   |   |   |   |    |   |   \   \  /   /    |   | |   |   \   \  |   |   |    |   |        "
-ping 127.0.0.1 -n 1.6 > nul
-echo "        |   |______|   |   |   |   |    |   |    \   \/   /     |   | |   |    \   \ |   |   |    |   |        "
-ping 127.0.0.1 -n 1.7 > nul
-echo "        |              |   |   |    \__/    |     \      /      |   | |   |     \   \|   |   |___/   /         "
-ping 127.0.0.1 -n 1.8 > nul
-echo "        |______________|   |___|\__________/       \____/       |___| |___|      \_______|__________/          "
-ping 127.0.0.1 -n 1.9 > nul
+echo "                               +___               ____+____________________+                                   "
+echo "                               |    \            /    |________    ________|                                   " 
+echo "                               |     \          /     |        |  |                                            "
+echo "                               |  |\  \        /  /|  |        |  |                                            "
+echo "                               |  | \  \      /  / |  |        |  |                                            "
+echo "                               |  |  \  \    /  /  |  |        |  |                                            "
+echo "                               |  |   \  \  /  /   |  |        |  |                                            "
+echo "                               |  |    \  \/  /    |  |        |  |                                            "
+echo "                               |  |     \    /     |  |        |  |                                            "
+echo "                               |__|      \__/      |__|        |__|                                            "
 echo "                                                                                                               "
-ping 127.0.0.1 -n 1.8 > nul
 echo "                                                                                                               "
-ping 127.0.0.1 -n 1.7 > nul
 echo """"""                                                                                                     """"""
-ping 127.0.0.1 -n 1.6 > nul
 set /p playlist="Paste url here : "
-ping 127.0.0.1 -n 1.7 > nul
 call "src/data_fetcher.py" %playlist%
 taskkill /f /im chrome.exe
 start /d "C:\Program Files (x86)\Internet Download Manager" IDMan.exe
